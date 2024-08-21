0f4f9845a9c0c001569abcd4544a43ae054e5c10
casicodes <sendtokcabhi@gmail.com>
2024-08-21 16:57:44 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProfileCard from "./components/ProfileCard";
+
+function App() {
+  return (
+    <>
+      <ProfileCard restroname="Jedan Pizza" />
+    </>
+  );
+}
+
+export default App;
