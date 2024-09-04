444757c6490e11c200aefdb9c4437802f87d8a9c
Abhishek KC <sendtokcabhi@gmail.com>
2024-09-04 17:04:25 +1000

    Add remaining functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import FarAway from "./components/FarAway";
 function App() {
   return (
     <div className="flex flex-col gap-14 max-w-[450px] mx-auto">
+      <FarAway title="Mini App" />
       <Menu title="Conditional rendering" />
       <Stepper title="React hook in practise" />
-      <FarAway title="Mini App" />
     </div>
   );
 }
--- src/components/FarAway.tsx
+++ src/components/FarAway.tsx
@@ -1,30 +1,36 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Sleeping bags", packed: true, quantity: 2 },
-  { id: 2, description: "Cookware", packed: false, quantity: 1 },
-  { id: 3, description: "Firewood", packed: false, quantity: 5 },
-];
 function FarAway({ title }) {
+  const [items, setItems] = useState([]);
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
-    const newItem = { id: Date.now(), description, packed: false, quantity };
-    console.log(newItem);
-    setQuantity(() => 1);
-    setDescription(() => "");
     e.preventDefault();
+    if (!description) return;
+    const newItem = { id: Date.now(), description, packed: false, quantity };
+    setItems((items) => [...items, newItem]);
+    setQuantity(1);
+    setDescription("");
+  }
+
+  function toggleStatus(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
   }
 
   return (
     <div className="flex flex-col gap-4 text-left">
       <p className="uppercase text-xs tracking-wider text-gray-400">{title}</p>
-      <div className="flex justify-between gap-2">
-        <form action="" onSubmit={handleSubmit}>
+      <div>
+        <form onSubmit={handleSubmit} className="flex justify-between gap-2">
           <select
-            name=""
-            id=""
-            className="border py-2 px-3 grow"
+            name="qty"
+            id="qty"
+            className="rounded-md border py-2 px-3 grow"
             value={quantity}
             onChange={(e) => setQuantity(Number(e.target.value))}
           >
@@ -37,7 +43,7 @@ function FarAway({ title }) {
           <input
             type="text"
             placeholder="Add item"
-            className="border py-2 px-3 grow"
+            className="rounded-md border py-2 px-3 grow"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
           />
@@ -47,20 +53,44 @@ function FarAway({ title }) {
         </form>
       </div>
       <div>
-        <ul>
-          {initialItems.map((item, id) => (
-            <Item item={item} key={id} />
+        <ul className="mt-4 divide-y">
+          {items.map((item) => (
+            <Item item={item} key={item.id} toggleStatus={toggleStatus} />
           ))}
         </ul>
       </div>
+      {items.length > 0 ? (
+        <div>
+          <button
+            className="border w-full py-2 px-3 rounded-full hover:bg-slate-50 grow"
+            onClick={() => setItems([])}
+          >
+            Clear all
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-function Item({ item }) {
+
+function Item({ item, toggleStatus }) {
   return (
-    <li className={item.packed ? "line-through" : ""}>
-      {item.description} ({item.quantity})
+    <li className="flex justify-between items-center py-2 first-of-type:pt-0">
+      <span
+        className={`${item.packed ? "line-through" : ""} flex justify-between`}
+      >
+        {item.quantity} x {item.description}
+      </span>{" "}
+      <button
+        className="border py-2 px-2 rounded-md hover:bg-slate-50"
+        onClick={() => toggleStatus(item.id)}
+      >
+        {item.packed ? "Undo" : "Done"}
+      </button>
     </li>
   );
 }
+
 export default FarAway;
