e2eb058a397e475e911b5952e5d4802ba3e97229
Abhishek KC <sendtokcabhi@gmail.com>
2024-10-19 23:51:56 +1100

    Remove component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import RecordTransaction from "./components/RecordTransaction.jsx";
 function App() {
   return (
     <div className="flex flex-col max-w-[480px] mx-auto gap-8">
-      <RecordTransaction title="Record your transaction" />
+      {/* <RecordTransaction title="Record your transaction" /> */}
       <Accordion title="Accordion using props" />
       <FarAway title="Mini app : Pack & track" />
       <Stepper title="React hook in practise" />
