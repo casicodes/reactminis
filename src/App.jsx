44bfad2c4e61d4e88c5a32752ed3675c147cbf0e
Abhishek KC <sendtokcabhi@gmail.com>
2024-10-19 23:21:00 +1100

    Add record transactions

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Stepper from "./components/Stepper.jsx";
 import FarAway from "./components/FarAway.jsx";
 import Accordion from "./components/Accordion.jsx";
+import RecordTransaction from "./components/RecordTransaction.jsx";
 
 function App() {
   return (
     <div className="flex flex-col max-w-[480px] mx-auto gap-8">
+      <RecordTransaction title="Record your transaction" />
       <Accordion title="Accordion using props" />
       <FarAway title="Mini app : Pack & track" />
       <Stepper title="React hook in practise" />
---
+++ src/components/RecordTransaction.jsx
@@ -0,0 +1,58 @@
+const initialItems = [
+  {
+    icon: "💵",
+    category: "paycheck",
+    expense: false,
+    amount: 1500,
+    time: "Mon 14 Oct",
+  },
+  {
+    icon: "🍎",
+    category: "grocery",
+    expense: true,
+    amount: 81,
+    time: "Sat 12 Oct",
+  },
+  {
+    icon: "🚕",
+    category: "transport",
+    expense: true,
+    amount: 15,
+    time: "Fri 11 Oct",
+  },
+];
+function RecordTransaction({ title }) {
+  const transactions = initialItems;
+  return (
+    <div className="flex flex-col gap-4 text-left bg-white p-4 rounded-md">
+      <p className="uppercase text-xs tracking-wider text-gray-400">{title}</p>
+      <div>
+        <ul>
+          {transactions.map((transaction) => (
+            <li className="flex flex-col gap-2 mb-4">
+              <span>{transaction.time}</span>
+              <div className="flex flex-row items-center gap-1">
+                <span className="text-2xl">{transaction.icon}</span>
+                <span className="flex flex-col grow">
+                  <span className="capitalize font-bold">
+                    {transaction.category}
+                  </span>
+                </span>
+                <span
+                  className={`${
+                    transaction.expense ? "text-red-500" : "text-black"
+                  } text-lg`}
+                >
+                  {transaction.expense
+                    ? `-${transaction.amount}`
+                    : `+${transaction.amount}`}
+                </span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default RecordTransaction;
